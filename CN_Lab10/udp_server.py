c5706a7290a3fd59ef756deabf1bf9453dfbc0f4
akankshaladdha <53806569+akankshaladdha@users.noreply.github.com>
2020-12-22 15:01:07 +0530

    Update udp_server.py

--- CN_Lab10/udp_server.py
+++ CN_Lab10/udp_server.py
@@ -1,27 +1,22 @@
-import socket
-
-localIP = "127.0.0.1"
-localPort = 65198
-bufferSize = 1024
-serverMsg = "Hello UDP Client!"
-bytesToSend = str.encode(serverMsg)
+from socket import *
+serverPort = 65198
 
 # Create a datagram socket
-UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket = socket(AF_INET, SOCK_DGRAM)
 
 # Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
-print("The UDP server is up and listening!")
+serverSocket.bind(("127.0.0.1", serverPort))
+
+print("The server is ready to receive: UDP")
 
 # Listen for incoming datagrams
+while 1:
+    sentence, clientAddress = serverSocket.recvfrom(2048)
 
-while (True):
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0]
-    address = bytesAddressPair[1]
-    clientMsg = "The message from Client: {}".format(message)
-    clientIP = "Client IP Address: {}".format(address)
-    print(clientMsg)
-    print(clientIP)
+    file = open(sentence, "r")
+    l = file.read(2048)
+
     # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
+    serverSocket.sendto(bytes(l, "utf-8"), clientAddress)
+    print("Sent back to client", l)
+file.close()
