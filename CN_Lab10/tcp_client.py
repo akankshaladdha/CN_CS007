f9ce5758ae32566c0166811dffe751e402ce2d6e
akankshaladdha <53806569+akankshaladdha@users.noreply.github.com>
2020-12-22 15:05:55 +0530

    Update tcp_client.py

--- CN_Lab10/tcp_client.py
+++ CN_Lab10/tcp_client.py
@@ -1,21 +1,15 @@
-import socket
+from socket import *
 
 serverName = '127.0.0.1'
-serverPort = 12345
+serverPort = 25109
 
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client_socket.connect((serverName,serverPort))
-payload='Hello TCP Server!'
-try:
-    while True:
-        client_socket.send(payload.encode('utf-8'))
-        data = client_socket.recv(1024)
-        print(str(data))
-        more = input('Do you wish to send more data to the Server?\n')
-        if more.lower() == 'y':
-            payload = input("Enter the payload\n")
-        else:
-            break
-except KeyboardInterrupt:
-    print("User Exit!")
-client_socket.close()
+#create socket
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect((serverName, serverPort))
+
+sentence = input("Enter file name: \n")
+
+clientSocket.send(sentence.encode())
+filecontents = clientSocket.recv(1024).decode()
+print('File contents from Server:\n', filecontents)
+clientSocket.close()
