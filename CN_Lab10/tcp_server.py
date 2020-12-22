18c545d79cbb8a4d488579d45dd00c3c3c569f61
akankshaladdha <53806569+akankshaladdha@users.noreply.github.com>
2020-12-22 15:03:08 +0530

    Update tcp_server.py

--- CN_Lab10/tcp_server.py
+++ CN_Lab10/tcp_server.py
@@ -1,28 +1,26 @@
-import socket
+from socket import *
 
 serverName = '127.0.0.1'
-serverPort = 12345
+serverPort = 25109
 
-#create server
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#create
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#bind server 
-server_socket.bind((serverName, serverPort ))
+#bind
+serverSocket.bind((serverName, serverPort))
 
 #listen
-server_socket.listen(5)
+serverSocket.listen(1)
 
-while True:
-    print("The Server is waiting for connection")
-    client_socket, addr = server_socket.accept()
-    print("The Client has been connected from",addr)
-    while True:
-        data = client_socket.recv(1024)
-        if not data or data.decode('utf-8') == 'END':
-            break
-        print("Received from Client : %s"%data.decode("utf-8"))
-        try:
-            client_socket.send(bytes('Hello TCP Client!', 'utf-8'))
-        except:
-            print("User Exit!")
-    client_socket.close()
+print("The server is ready to receive: TCP")
+while 1:
+    connectionSocket, addr = serverSocket.accept()
+    print("Client has been connected from : ",addr)
+    sentence = connectionSocket.recv(1024).decode()
+
+    file = open(sentence, "r")
+    l = file.read(1024)
+
+    connectionSocket.send(l.encode())
+    file.close()
+    connectionSocket.close()
